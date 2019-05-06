09dada704768746a422793f85cb75f2c215ce330
Jake Vasilakes <vasil024@umn.edu>
2019-05-06 15:58:43 -0500

    Update docs and added set cover initializatins.

--- pall/initializations.py
+++ pall/initializations.py
@@ -1,7 +1,9 @@
 import warnings
 import numpy as np
+import cvxpy as cp
 from scipy import sparse
 from sklearn.neighbors import NearestNeighbors
+from collections import Counter
 
 
 class CentralityMeasure(object):
@@ -56,6 +58,11 @@ class CentralityMeasure(object):
             scores[x_i] = score / (self.k - 1)
         return scores
 
+    def find_centers(self, n=50):
+        self.scores = self.centrality()
+        # Return indices of the N data points with the highest scores.
+        return np.argsort(self.scores)[::-1][:n]
+
 
 class ClosenessCentrality(CentralityMeasure):
     """
@@ -63,8 +70,7 @@ class ClosenessCentrality(CentralityMeasure):
     """
 
     def __init__(self, X, k=30):
-        super().__init__(X, k)
-        # Use the distance matrix
+        super().__init__(X, k)  # Computes the distance graph
 
     def weight(self, i, j):
         dist = self.D[i, j]
@@ -85,9 +91,7 @@ class DegreeCentrality(CentralityMeasure):
     """
 
     def __init__(self, X, k=30):
-        super().__init__(X, k)
-        # Use the adjacency matrix
-        self.graph = self.A
+        super().__init__(X, k)  # Computes the adjacency graph
 
     def weight(self, i, j):
         # 1 if i is connected to j, else 0.
@@ -114,6 +118,7 @@ class EigenvectorCentrality(CentralityMeasure):
         else:
             self.n = int(n)
 
+    # Overload base centrality.
     def centrality(self):
         vals, vecs = sparse.linalg.eigs(self.A.asfptype(), k=self.n)
         return vals
@@ -125,13 +130,129 @@ class LDSCentrality(CentralityMeasure):
     """
 
     def __init__(self, X, k=30):
-        super().__init__(X, k)
+        super().__init__(X, k)  # Compute the adjacency graph
 
     def weight(self, i, j):
-        # Because graph is the adjacency matrix,
-        # this is the same as the intersection.
-        # Elementwise multiplication
+        # Because this is the adjacency matrix, elementwise multiplication
+        # is the same as the intersection.
         return np.sum(self.A[i, ].multiply(self.A[j, ]))
+
+
+class SetCover(object):
+
+    def __init__(self, X, k=30):
+        self.X = X
+        self.k = int(k)
+        self.neighbors, self.distances = self._KNN()
+        self.cover = None
+
+    def _KNN(self):
+        NN = NearestNeighbors(n_neighbors=self.k, algorithm="brute",
+                              metric="minkowski")
+        NN.fit(self.X)
+        dists, neighbors = NN.kneighbors()
+        return neighbors, dists
+
+    def find_centers(self, n=50):
+        raise NotImplementedError
+
+
+class GreedySetCover(SetCover):
+    """
+    Given a set of partial covers :math:`S` of :math:`X`,
+    greedily search for a subset of them, indexed by :math:`I`
+    such that :math:`\\bigcup_{i \\in I} S_i~ = X`
+    """
+
+    def __init__(self, X, k=30):
+        super().__init__(X, k)
+
+    def _set_cover(self):
+        nns_set = [set(n) for n in self.neighbors]
+        elements = set.union(*nns_set)
+        covered = set()
+        cover = []
+        while covered != elements:
+            subset = max(nns_set, key=lambda s: len(s - covered))
+            cover.append(list(subset))
+            covered |= subset
+        return np.array(cover)
+
+    def find_centers(self, n=50):
+        self.cover = self._set_cover()
+        counts = Counter(self.cover.ravel())
+        counts = sorted(counts.items(), key=lambda x: x[1], reverse=True)
+        # The indices of the first n sorted counts.
+        return np.array(counts)[:n, 0]
+
+
+class FacilityLocation(SetCover):
+    """
+    This is a simplified version of the uncapacitated facility
+    location problem in which there is no cost to open a facility.
+    Customers are data points and facilities are centers. The cost
+    to ship from a facility to a customer is computed as the distance
+    between them in a k nearest neighbor graph.
+
+    :math:`I` : Set of candidate center locations.
+
+    :math:`J` : Set of data points.
+
+    N.B. In this case :math:`I ~=~ J` as each data point is a potential center.
+
+    :math:`M` : Maximum number of centers.
+
+    .. math::
+
+        y_{ij} =
+        \\begin{cases}
+          1 & \\text{if center} ~i~ \\text{covers data point} ~j
+
+          0 & \\text{otherwise}
+        \\end{cases}
+
+    :math:`D_{ij} =` distance between center :math:`i` and data point :math:`j`
+
+    :math:`\\epsilon =` number of permissable outliers
+
+    minimize :math:`\\sum_{i \\in I} \\sum_{j \\in J} D_{ij} y_{ij}`
+
+    subject to
+
+                :math:`\\sum_i max_j ~y_{ij} \leq M`
+
+                :math:`\\sum_{ij} y_{ij} = ~|J| - ~\\epsilon`
+
+                :math:`y_{ij} \\in \\{0,1\\} ~~\\forall i \\in I, j \\in J`
+
+    """
+
+    def __init__(self, X, k=30):
+        super().__init__(X, k)
+
+    def _distance_matrix(self):
+        n = self.neighbors.shape[0]
+        D = np.zeros((n, n), dtype=float)
+        for (i, js) in enumerate(self.neighbors):
+            D[i, js] = self.distances[i]
+        return D
+
+    def find_centers(self, n=50, epsilon="auto"):
+        if epsilon == "auto":
+            epsilon = int(np.ceil(1e-4 * X.shape[0]))
+        else:
+            epsilon = int(epsilon)
+        D = self._distance_matrix()
+        y = cp.Variable(D.shape, boolean=True)
+        cost = cp.sum(cp.multiply(D, y))
+        constraints = [cp.sum(y) == D.shape[0],
+                       cp.sum(cp.max(y, axis=1)) <= n - epsilon]
+        prob = cp.Problem(cp.Minimize(cost), constraints)
+        prob.solve(solver="GUROBI")
+
+        y_int = np.around(y.value)
+        centers = np.argwhere(np.max(y_int, axis=1) == 1).ravel()
+        return centers
 
 
 if __name__ == "__main__":
@@ -143,9 +264,14 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset", type=str, default="iris",
-                        choices=["breast_cancer", "dexter", "digits", "iris"])
-    parser.add_argument("--metric", type=str, default="degree",
-                        choices=["closeness", "degree", "eigen", "lds"])
+                        choices=["breast_cancer", "dexter", "digits", "iris"],
+                        help="Which data set to use.")
+    parser.add_argument("--method", type=str, default="degree",
+                        choices=["closeness", "degree", "eigen", "lds",
+                                 "greedy", "facloc"],
+                        help="Which method to use.")
+    parser.add_argument("--n", type=int, default=50,
+                        help="Number of initial data points to find.")
     args = parser.parse_args()
 
     np.random.seed(0)
@@ -165,24 +291,29 @@ if __name__ == "__main__":
     y = dataset.target
 
     K = 20
-    N = 50
-    if args.metric == "closeness":
+    if args.method == "closeness":
         cm = ClosenessCentrality(X, K)
-    elif args.metric == "degree":
+    elif args.method == "degree":
         cm = DegreeCentrality(X, K)
-    elif args.metric == "eigen":
-        cm = EigenvectorCentrality(X, K, n=N)
-    elif args.metric == "lds":
+    elif args.method == "eigen":
+        cm = EigenvectorCentrality(X, K, n=args.n)
+    elif args.method == "lds":
         cm = LDSCentrality(X, K)
+    elif args.method == "greedy":
+        cm = GreedySetCover(X, K)
+    elif args.method == "facloc":
+        cm = FacilityLocation(X, K)
+    else:
+        raise ValueError(f"Unknown method '{args.method}'.")
 
-    X = TSNE(n_components=2).fit_transform(X)
-    scores = cm.centrality()
-    idxs = np.argsort(scores)[::-1][:N]
     print(f"K: {K}")
-    print(f"N: {N}")
-    print(f"Scores: {scores.max():.3f} {scores.min():.3f}")
+    print(f"N: {args.n}")
+    idxs = cm.find_centers(n=args.n)
+    print(f"Found {len(idxs)} centers.")
+
     mask = np.zeros(X.shape[0], dtype=bool)
     mask[idxs] = True
+    X = TSNE(n_components=2).fit_transform(X)
     LX = X[mask, ]
     UX = X[np.logical_not(mask), ]
     Ly = y[mask]
